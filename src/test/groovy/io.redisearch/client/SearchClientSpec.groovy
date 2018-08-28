ba042eb71a147dec8285638f26904af29ecd6774
David Parry <dparry@mutualink.net>
2018-08-28 14:47:10 -0500

    more tests including search which captured the connected SearchResult

--- src/test/groovy/io.redisearch/client/SearchClientSpec.groovy
+++ src/test/groovy/io.redisearch/client/SearchClientSpec.groovy
@@ -1,17 +1,19 @@
 package io.redisearch.client
 
 import io.redisearch.Query
+import io.redisearch.SearchResult
 import io.redisearch.api.SearchClient
 
 class SearchClientSpec extends ClientSpec {
 
+    String indexName = "testIndex"
 
     def setup() {
 
     }
 
     def "explain test explain is called"() {
-        String indexName = "testIndex"
+
         String statusCodeReply = "INTERSECT {\n" +
                 "  @f3:UNION {\n" +
                 "    @f3:f3_val\n" +
@@ -41,5 +43,59 @@ class SearchClientSpec extends ClientSpec {
         result == statusCodeReply
     }
 
+
+    def "basic search for word with no content"() {
+        String id = "record:id"
+
+        when:
+        Query query = new Query("search text")
+        query.setNoContent()
+        SearchClient client = new Client(indexName, pool)
+        SearchResult result = client.search(query)
+
+        then:
+        1 * binaryClient.getObjectMultiBulkReply() >> [1L, id.getBytes()]
+        1 * binaryClient.sendCommand(Commands.Command.SEARCH, _)
+        result.totalResults == 1
+        result.docs.get(0).getId() == id
+    }
+
+    def "basic search for word with content"() {
+        String id = "record:id"
+        def value = ["title".getBytes(), "here is a value".getBytes()]
+
+        when:
+        Query query = new Query("search text")
+        SearchClient client = new Client(indexName, pool)
+        SearchResult result = client.search(query)
+
+        then:
+        1 * binaryClient.getObjectMultiBulkReply() >> [1L, id.getBytes(), value]
+        1 * binaryClient.sendCommand(Commands.Command.SEARCH, _)
+        result.totalResults == 1
+        result.docs.get(0).getId() == id
+    }
+
+    def "basic search for word with payload and score"() {
+        String id = "record:id"
+        String payload = "I am a payload"
+        def content = ["title".getBytes(), "here is a value".getBytes()]
+        String score = "4.445670127868652"
+
+        when:
+        Query query = new Query("search text")
+        query.setWithPaload()
+        query.setWithScores()
+        SearchClient client = new Client(indexName, pool)
+        SearchResult result = client.search(query)
+
+        then:
+        1 * binaryClient.getObjectMultiBulkReply() >> [1L, id.getBytes(), score.getBytes(), payload.getBytes(), content]
+        1 * binaryClient.sendCommand(Commands.Command.SEARCH, _)
+        result.totalResults == 1
+        result.docs.get(0).getId() == id
+        result.docs.get(0).getPayload() == payload.getBytes()
+        result.docs.get(0).getScore() == Double.parseDouble(score)
+    }
 
 }

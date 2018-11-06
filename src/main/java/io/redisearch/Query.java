7335fa8a58a0d385d0fc4e24b6e4b7b575168154
Guy Korland <gkorland@gmail.com>
2018-11-06 22:20:30 +0200

    Fix PAYLOADS should be WITHPAYLOADS

--- src/main/java/io/redisearch/Query.java
+++ src/main/java/io/redisearch/Query.java
@@ -191,7 +191,7 @@ public class Query {
             args.add("WITHSCORES".getBytes());
         }
         if (_withPayloads) {
-            args.add("PAYLOADS".getBytes());
+            args.add("WITHPAYLOADS".getBytes());
         }
         if (_language != null) {
             args.add("LANGUAGE".getBytes());

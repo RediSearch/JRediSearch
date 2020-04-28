03aa4966da44a4c6b4e71113edb3ddece798e6d8
Tugdual Grall <tugdual@gmail.com>
2020-04-28 15:01:20 +0200

    Issue 91 null values (#92)
    * manage null values in Document attribute, raising JedisDataException with clear message instead of NPE
    
    * mend
    
    manage null values in Document attribute, raising JedisDataException with clear message instead of NPE
    
    * After @gkorkland review and moving to NPE with custom message - issue #91
    
    Co-authored-by: Guy Korland <gkorland@gmail.com>

--- src/main/java/io/redisearch/client/Client.java
+++ src/main/java/io/redisearch/client/Client.java
@@ -512,9 +512,14 @@ public class Client implements io.redisearch.Client {
         }
 
         args.add(Keywords.FIELDS.getRaw());
+        String key = null;
         for (Map.Entry<String, Object> ent : doc.getProperties()) {
-            args.add(SafeEncoder.encode(ent.getKey()));
+            key = ent.getKey();
+            args.add(SafeEncoder.encode(key));
             Object value = ent.getValue();
+            if (value == null) {
+                throw new NullPointerException( "Document attribute '"+ key +"' is null. (Remove it, or set a value)" );
+            }
             args.add(value instanceof byte[] ?  (byte[])value :  SafeEncoder.encode(value.toString()));
         }
 

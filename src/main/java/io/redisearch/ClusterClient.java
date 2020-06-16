a1120c6ac02d6a242f4b3860df892213353a622b
Guy Korland <gkorland@gmail.com>
2020-06-16 17:47:12 +0300

    deprecate ClusterClient

--- src/main/java/io/redisearch/ClusterClient.java
+++ src/main/java/io/redisearch/ClusterClient.java
@@ -2,6 +2,10 @@ package io.redisearch;
 
 import java.util.List;
 
+/**
+ * @deprecated ClusterClient is going to be removed in the future
+ */
+@Deprecated
 public interface ClusterClient extends Client {
 
     List<Object> broadcast(String... args);
